fc8349d91938f052555e02b128ca1de41928e123
Yevgen <107062952+Yevgen1984@users.noreply.github.com>
2022-10-23 23:52:41 +0300

    exp 5

---
+++ src/Pages/ReviewPage.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Loader } from '../components/Loader/Loader';
+import { ReviewList } from '../components/Reviews/Reviews';
+import { MoviesAPI } from '../servises/MoviesApi';
+
+const ReviewPage = () => {
+  const [movieInfo, setMovieInfo] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    const getMovie = async movieId => {
+      setIsLoading(true);
+      try {
+        const movieDetails = await MoviesAPI.fetchMovieReviews(movieId);
+        setMovieInfo(movieDetails);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getMovie(movieId);
+  }, [movieId]);
+  return (
+    <>
+      {isLoading && <Loader />}
+      {error && <p>We don't have any reviews for this movie</p>}
+      {movieInfo && <ReviewList movieInfo={movieInfo} />}
+    </>
+  );
+};
+
+export default ReviewPage;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,27 @@
 import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
-// import { lazy, Suspense } from 'react';
-// import { Home } from '../pages/HomePage';
-// import Movies  from '../Pages/Movies';
-// import MoviesDetails from '../Pages/MoviesDetails';
-// import CastPage from '../Pages/CastPage';
-// import ReviewsPage from '../Pages/ReviewsPage';
 
 const HomePage = lazy(() => import('../pages/HomePage'));
 const Movies = lazy(() => import('../pages/Movies'));
 const MoviesDetails = lazy(() => import('../pages/MoviesDetails'));
 
 const CastPage = lazy(() => import('../pages/CastPage'));
-const ReviewsPage = lazy(() => import('../pages/ReviewsPage'));
+const ReviewPage = lazy(() => import('../pages/ReviewPage'));
 
 export const App = () => {
   return (
     <>
-      {/* <Suspense> */}
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="movies" element={<Movies />} />
-            <Route path="movies/:movieId" element={<MoviesDetails />}>
-              <Route path="cast" element={<CastPage />} />
-              <Route path="reviews" element={<ReviewsPage />} />
-            </Route>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MoviesDetails />}>
+            <Route path="cast" element={<CastPage />} />
+            <Route path="reviews" element={<ReviewPage />} />
           </Route>
-        </Routes>
-      {/* </Suspense> */}
+        </Route>
+      </Routes>
     </>
   );
 };
